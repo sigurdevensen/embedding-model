d1800d149170a6cea7228657598847009c04054c
Sigurd <sigurev@stud.ntnu.no>
2026-09-21 16:05:19 +0200

    feat(persistance): save output models to folder

--- src/utils/persistance.py
+++ src/utils/persistance.py
@@ -1,11 +1,13 @@
 import numpy as np
+import os
 
 def save_vocab(word2idx, idx2word, prefix="vocab"):
-    np.save(f"{prefix}_word2idx.npy", word2idx)
-    np.save(f"{prefix}_idx2word.npy", idx2word)
+    os.makedirs("out", exist_ok=True)
+    np.save(f"out/{prefix}_word2idx.npy", word2idx)
+    np.save(f"out/{prefix}_idx2word.npy", idx2word)
 
 
 def load_vocab(prefix="vocab"):
-    word2idx = np.load(f"{prefix}_word2idx.npy", allow_pickle=True).item()
-    idx2word = np.load(f"{prefix}_idx2word.npy", allow_pickle=True).item()
+    word2idx = np.load(f"out/{prefix}_word2idx.npy", allow_pickle=True).item()
+    idx2word = np.load(f"out/{prefix}_idx2word.npy", allow_pickle=True).item()
     return word2idx, idx2word
